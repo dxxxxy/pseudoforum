1347ffe9c38c6c3949a522517e36bebcc93957cf
ECherik <ya8687@hotmail.ca>
2022-11-29 16:54:13 -0500

    created AdminBox

---
+++ src/lib/AdminBox.jsx
@@ -0,0 +1,13 @@
+function AdminBox () {
+    return(
+        <div id="admin-div">
+            <p>ADMIN</p>
+            <button>Create Category</button>
+            <button>Create Topic</button>
+            <button>Close topic</button>
+            <button>Delete Topic</button>
+        </div>
+    )
+}
+
+export default AdminBox
